37e54a4d0fcdf4ef9a90f2e6da0a3510fcab1b0e
Patryk Baraniak <padajo12@gmail.com>
2025-02-06 04:50:01 +0100

    vercel issues

--- src/app/obliczTrase+/camera.tsx
+++ src/app/obliczTrase+/camera.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import OcrSpaceReader from "./ocr-space-api-wrapper";
 import Image from "next/image";
 
