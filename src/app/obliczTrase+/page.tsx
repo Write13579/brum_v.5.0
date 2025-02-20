574131ad707539f793a45bc4806fbd899d4fce90
Patryk Baraniak <padajo12@gmail.com>
2025-02-20 11:56:44 +0100

    vercel issues2

--- src/app/obliczTrase+/page.tsx
+++ src/app/obliczTrase+/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/lib/database";
 import { InputyDlaTrasy } from "./InputyDlaTrasy";
 
+export const dynamic = "force-dynamic"; //vercel sie cos pultal
+
 export default async function page() {
   async function pobierzTrasy() {
     const trasy = await db.query.odleglosci.findMany();
